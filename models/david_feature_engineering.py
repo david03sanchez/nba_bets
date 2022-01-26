c1cc48ebc17c466860821028e4234b413c211834
David <david@david.david>
2022-01-26 12:10:39 -0500

    linear regression

--- models/david_feature_engineering.py
+++ models/david_feature_engineering.py
@@ -3,21 +3,55 @@ import numpy as np
 from multiprocessing import get_context
 import xgboost as xgb
 from sklearn.metrics import mean_squared_error, mean_absolute_error
-
 df1 = pd.read_csv('/home/danny/nba/data/gamedf.csv', index_col=0)
+df1.index = pd.to_datetime(df1['GAME_DATE'])
+df1.sort_index(inplace=True, ascending=False)
+distance_df = pd.read_csv('/home/data/distance_map.csv', index_col=0)
+#%%
+def distanceSlice(game_id, team):
+    #TEAM_ABBREVIATION, GAME_ID, GAME_DATE, MATCHUP, DISTANCE
+    # game_id = 22100560
+    # team = 'ATL'
+    subset = df1.loc[:, ['TEAM_ABBREVIATION', 'GAME_ID', 'GAME_DATE', 'MATCHUP']][df1['GAME_ID'] == game_id]
+    game_date = pd.to_datetime(subset['GAME_DATE'][0])
+    subset['distance'] = np.nan
+    hometeam = subset[subset['MATCHUP'].str.contains('@') == False].iloc[0, 0]
+    awayteam = subset[subset['MATCHUP'].str.contains('@') == True].iloc[0, 0]
+    # get all games of hometeam, locate last 2, ['MATCHUP'][1] is previous game, check if home/away
+    homeAwayCheck = True if '@' in df1[(df1['TEAM_ABBREVIATION'] == hometeam) & (pd.to_datetime(df1['GAME_DATE']) <= game_date)].iloc[:2, :]['MATCHUP'][1] else False
+    awayAwayCheck = True if '@' in df1[(df1['TEAM_ABBREVIATION'] == awayteam) & (pd.to_datetime(df1['GAME_DATE']) <= game_date)].iloc[:2, :]['MATCHUP'][1] else False
+    # homeDistance = 0 if homeAwayCheck is False else
+    # if hometeam was home last game
+    if homeAwayCheck is False:
+        homeDistance = 0
+    # if hometeam was away last game
+    elif homeAwayCheck is True:
+        # where the home team played last game
+        prevGameLoc = df1[(df1['TEAM_ABBREVIATION'] == hometeam) & (pd.to_datetime(df1['GAME_DATE']) <= game_date)].iloc[:2, :]['MATCHUP'][1][-3:]
+        homeDistance = distance_df[distance_df['regular'].str.contains(f'{hometeam}{prevGameLoc}')]['distance'].reset_index(drop=True)[0]
+
+    # if awayteam was home last game
+    if awayAwayCheck is False:
+        awayDistance = 0
+    elif awayAwayCheck is True:
+        prevGameLoc = df1[(df1['TEAM_ABBREVIATION'] == awayteam) & (pd.to_datetime(df1['GAME_DATE']) <= game_date)].iloc[:2, :]['MATCHUP'][1][-3:]
+        awayDistance = distance_df[distance_df['regular'].str.contains(f'{hometeam}{prevGameLoc}')]['distance'].reset_index(drop=True)[0]
+    subset.loc[subset['TEAM_ABBREVIATION'] == hometeam, 'distance'] = homeDistance
+    subset.loc[subset['TEAM_ABBREVIATION'] == awayteam, 'distance'] = awayDistance
+    # .reset_index(drop=True, inplace=True)
+    return subset[subset['TEAM_ABBREVIATION'] == team].iloc[0,-1]
 
 def getTeamStats(abv, latestdate):
-    abv = 'ATL'
-    latestdate = '2003-12-14'
-    # edit this function for additional feature eng
+    # abv = 'ATL'
+    # latestdate = '2021-01-06'
     team_subset = df1[(df1['TEAM_ABBREVIATION'] == abv) & (pd.to_datetime(df1['GAME_DATE']) < latestdate)].copy()
     team_subset['GAME_DATE'] = pd.to_datetime(team_subset['GAME_DATE'])
     team_subset.index = team_subset['GAME_DATE']
     team_subset.sort_index(inplace=True, ascending=False)
-    stats_columns = ['PTS', 'FGM', 'FGA', 'FG_PCT',
+    stats_columns = ['GAME_ID', 'PTS', 'FGM', 'FGA', 'FG_PCT',
                      'FG3M', 'FG3A', 'FG3_PCT', 'FTM', 'FTA', 'FT_PCT', 'OREB', 'DREB',
                      'REB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'window_sum10', 'window_sum5',
-                     'window_sum3', 'location', 'numerical_wins', 'break_days', 'PIE', 'CORE_PTS']
+                     'window_sum3', 'location', 'numerical_wins', 'break_days', 'distance', 'PIE', 'CORE_PTS']
     team_subset['numerical_wins'] = np.where(team_subset['WL'] == 'L', 0, 1)
     team_subset['location'] = np.where(team_subset['MATCHUP'].str.contains('@'), -1, 1)
     team_subset = team_subset.iloc[::-1].copy()
@@ -27,29 +61,34 @@ def getTeamStats(abv, latestdate):
     team_subset['LAG_DATA'] = team_subset['GAME_DATE'].shift(1)
     team_subset['break_days'] = team_subset["GAME_DATE"] - team_subset["LAG_DATA"]
     team_subset['break_days'] = team_subset['break_days'].dt.days
+    team_subset['distance'] = np.nan
     team_subset['PIE'] = (
         team_subset['PTS'] + team_subset['FGM'] + team_subset['FTM'] - team_subset['FTA'] +
         team_subset['DREB'] + team_subset['OREB'] + team_subset['AST'] + team_subset['STL'] +
         team_subset['BLK'] - team_subset['PF'] - team_subset['TOV'])
     team_subset['CORE_PTS'] = team_subset['PTS']
-    team_subset = team_subset.iloc[-11:, [team_subset.columns.get_loc(c) for c in stats_columns]]
-    team_subset.iloc[:, 0:18] = team_subset.iloc[:, 0:18].ewm(halflife=7).mean()
+    team_subset = team_subset.iloc[-60:, [team_subset.columns.get_loc(c) for c in stats_columns]]
+    # x = 2
+    for x in range(60):
+        game_row = team_subset.iloc[x, :]
+        game_row['distance'] = distanceSlice(game_row['GAME_ID'], abv)
+        team_subset.iloc[x, team_subset.columns.get_loc('distance')] = game_row['distance']
 
     return team_subset
 
 def getOverUnder(gameid):
     try:
         target_game = df1[df1['GAME_ID'] == gameid].reset_index(drop=True)  # contains target
-        # target_game = df1[df1['GAME_ID'] == 29900549].reset_index(drop=True) #contains target
+        # target_game = df1[df1['GAME_ID'] == 22100560].reset_index(drop=True) #contains target
         if target_game.shape[0] != 2: #if all-star or other special game
             return None
-        game_date = target_game.loc[0,'GAME_DATE']
-        over_under = target_game['PTS'].sum()
+        game_date = pd.to_datetime(target_game.loc[:,'GAME_DATE'].iloc[0])
+        over_under = target_game.loc[~target_game['MATCHUP'].str.contains('@')]['PTS'].iloc[0] - target_game.loc[target_game['MATCHUP'].str.contains('@')]['PTS'].iloc[0]
         home_team = target_game.loc[~target_game['MATCHUP'].str.contains('@')]['TEAM_ABBREVIATION'].iloc[0]
         away_team = target_game.loc[target_game['MATCHUP'].str.contains('@')]['TEAM_ABBREVIATION'].iloc[0]
         home_df = getTeamStats(home_team, game_date)
         away_df = getTeamStats(away_team, game_date)
-        if home_df.shape == (11, 26) and away_df.shape == (11, 26):
+        if home_df.shape == (11, 28) and away_df.shape == (11, 28):
             output = [game_date, over_under, home_df, away_df]
         else:
             return None
